de22b8ad558e4741257a3a157c601b5dcc84bddd
Rain <pyc0422@gmail.com>
2025-06-01 19:16:50 -0500

    update linkedin link

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,17 +1,34 @@
-import linkedin from '../images/linkedin.png';
-import github from '../images/github.png'
+import linkedin from "../images/linkedin.png";
+import github from "../images/github.png";
 
-function Links () {
+function Links() {
   return (
     <div>
-      <a href="https://www.linkedin.com/in/yuchen-rain-pan" style={{padding:1, margin:1}}>
-        <img className="scaled-img" height={40} width={40} src={linkedin} alt="linkedin" title="LinkedIn"/>
+      <a
+        href="https://www.linkedin.com/in/yuchen-raina-pan/"
+        style={{ padding: 1, margin: 1 }}
+      >
+        <img
+          className="scaled-img"
+          height={40}
+          width={40}
+          src={linkedin}
+          alt="linkedin"
+          title="LinkedIn"
+        />
       </a>
-      <a href="https://github.com/pyc0422" style={{padding:1,  margin:1}}>
-          <img className="scaled-img" height={40} width={40} src={github} alt="github" title="github"/>
+      <a href="https://github.com/pyc0422" style={{ padding: 1, margin: 1 }}>
+        <img
+          className="scaled-img"
+          height={40}
+          width={40}
+          src={github}
+          alt="github"
+          title="github"
+        />
       </a>
     </div>
-  )
+  );
 }
 
 export default Links;
